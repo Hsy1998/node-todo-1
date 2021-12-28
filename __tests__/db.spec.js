13b8607fff8fc823f37c7064d1b8d8df9547698d
a553331998@gmail.com <a553331998@gmail.com>
2021-12-28 21:47:45 +0800

    完成读写功能的单元测试

--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -4,10 +4,23 @@ const { parseExpectedArgs } = require('commander')
 jest.mock('fs')
 
 describe('db', () => {
+  afterEach(() => {
+    fs.clearMocks()
+  })
   it('can read', async () => {
     const data = [{ title: 'hi', done: true }]
-    fs.setMock('/xxx', null, JSON.stringify(data))
+    fs.setReadFileMock('/xxx', null, JSON.stringify(data))
     const list = await db.read('/xxx')
     expect(list).toStrictEqual(data)
+  })
+  it('can write', async () => {
+    let fakeFile = ''
+    fs.setWriteFileMock('/yyy', (path, data, callback) => {
+      fakeFile = data
+      callback(null)
+    })
+    const list = [{title: '见欧阳娜娜', done: true}, {title: '见迪丽热巴', done: true}]
+    await db.write(list, '/yyy')
+    expect(fakeFile).toBe(JSON.stringify(list) + '\n')
   })
 })
